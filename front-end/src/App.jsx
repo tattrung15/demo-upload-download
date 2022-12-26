d4f84882aff390ea54e9dc4f29549052b8dcd261
Tất Trung <buitrungt@gmail.com>
2022-12-26 17:36:40 +0700

    upload

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,8 +1,18 @@
-import { useMemo } from "react";
+import axios from "axios";
+import { useEffect, useMemo, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [file, setFile] = useState(null);
+  const [progress, setProgress] = useState(null);
+
   const controller = useMemo(() => new AbortController(), []);
+
+  useEffect(() => {
+    return () => {
+      controller.abort();
+    };
+  }, [controller]);
 
   const handleDownload = async () => {
     // axios
@@ -10,7 +20,6 @@ function App() {
     //     responseType: "blob",
     //     onDownloadProgress: (event) => {
     //       const progress = Math.round((100 * event.loaded) / event.total);
-    //       console.log(progress);
     //     },
     //     signal: controller.signal,
     //   })
@@ -22,16 +31,45 @@ function App() {
     }).click();
   };
 
+  const handleFileChange = (event) => {
+    const files = event.target.files;
+    if (files.length) {
+      setFile(files[0]);
+    } else {
+      setFile(null);
+    }
+  };
+
+  const handleUpload = () => {
+    if (!file) return;
+
+    axios
+      .post("http://localhost:5000/upload", file, {
+        headers: {
+          "Content-Type": "application/octet-stream",
+        },
+        onUploadProgress: (event) => {
+          const progress = Math.round((100 * event.loaded) / event.total);
+          setProgress(progress);
+        },
+        signal: controller.signal,
+      })
+      .then((res) => {
+        setProgress(null);
+      });
+  };
+
   return (
     <div className="App">
       <div>
-        <button>Upload</button>
+        <input type="file" name="" id="" onChange={handleFileChange} />
+        <button onClick={handleUpload}>Upload</button>
+      </div>
+      <div>
+        <div>{progress && `${progress}%`}</div>
       </div>
       <div>
         <button onClick={handleDownload}>Download</button>
-      </div>
-      <div>
-        <button onClick={() => controller.abort()}>Cancel Download</button>
       </div>
     </div>
   );
